b9c6dac417cd9a69c90471ce8f04883b17f48c6d
eoin raeside <eoin raeside>
2016-11-17 18:02:32 +0000

    added seek() and flee() to myproject

--- CarNeat/Assets/MyScene/SphereMove.cs
+++ CarNeat/Assets/MyScene/SphereMove.cs
@@ -2,21 +2,21 @@
 using System.Collections;
 
 public class SphereMove : MonoBehaviour {
-
+	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKeyDown (KeyCode.UpArrow))
-			this.gameObject.transform.Translate(Vector3.forward);	
-		if (Input.GetKeyDown (KeyCode.DownArrow))
-			this.gameObject.transform.Translate (Vector3.back);	
-		if (Input.GetKeyDown (KeyCode.LeftArrow))
-			this.gameObject.transform.Translate (Vector3.left);	
-		if (Input.GetKeyDown (KeyCode.RightArrow))
-			this.gameObject.transform.Translate (Vector3.right);	
+		if (Input.GetKey (KeyCode.UpArrow))
+			transform.Translate (Vector3.forward);	
+		if (Input.GetKey (KeyCode.DownArrow))
+			transform.Translate (Vector3.back);	
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Translate (Vector3.left);	
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Translate (Vector3.right);	
 	}
 }
---
+++ CarNeat/Assets/Steering/SteeringReynolds.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringReynolds : MonoBehaviour {
+
+	public float maxSpeed = 1.0f;
+	public GameObject target;
+	Vector3 look;
+	Rigidbody rb;
+	Vector3 desiredVelocity;
+	public float slowingRad = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Steer ();
+		Flee ();
+	}
+
+	void Steer(){
+		
+		float distance = Vector3.Magnitude (target.transform.position - transform.position);
+		desiredVelocity = (target.transform.position - transform.position).normalized * maxSpeed;
+
+		if (distance < slowingRad)
+			desiredVelocity = desiredVelocity * (distance / slowingRad);
+
+		Vector3 steering = desiredVelocity - rb.velocity;
+		steering = Vector3.ClampMagnitude (steering, maxSpeed);
+		steering = steering / rb.mass;
+		
+		rb.velocity = Vector3.ClampMagnitude (rb.velocity + steering, maxSpeed);
+		transform.LookAt (target.transform.position);
+	}
+
+	void Flee(){
+		desiredVelocity = ((target.transform.position - transform.position).normalized * maxSpeed) * -1;
+
+		Vector3 steering = desiredVelocity - rb.velocity;
+		steering = Vector3.ClampMagnitude (steering, maxSpeed);
+		steering = steering / rb.mass;
+
+		rb.velocity = Vector3.ClampMagnitude (rb.velocity + steering, maxSpeed);
+
+		Quaternion rotation = Quaternion.LookRotation (transform.position - target.transform.position);
+		transform.rotation = rotation;
+	}
+}
